bcf59043977d897a2fd89f3cec63620493151875
Sam Gammon <sam@elide.ventures>
2023-04-16 00:33:38 -0700

    fix: kotlin version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
-    id("org.jetbrains.kotlin.jvm") version "1.6.21"
-    id("org.jetbrains.kotlin.kapt") version "1.6.21"
-    id("org.jetbrains.kotlin.plugin.allopen") version "1.6.21"
+    id("org.jetbrains.kotlin.jvm") version "1.8.20"
+    id("org.jetbrains.kotlin.kapt") version "1.8.20"
+    id("org.jetbrains.kotlin.plugin.allopen") version "1.8.20"
     id("com.github.johnrengelman.shadow") version "7.1.2"
     id("io.micronaut.application") version "3.7.8"
     id("io.micronaut.test-resources") version "3.7.8"
