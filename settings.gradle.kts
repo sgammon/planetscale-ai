bf27104b70dd745b4d50c419ed3c43309629bf8b
Sam Gammon <sam@elide.ventures>
2023-04-16 05:39:33 -0700

    feat: add remote build caching with buildless
    learn more at https://less.build

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,41 @@
 
 rootProject.name="planetscale-ai"
 
+val enableLocalCache = (System.getenv("GRADLE_CACHE_LOCAL")?.toBoolean() ?: true)
+val enableRemoteCache = (System.getenv("GRADLE_CACHE_REMOTE")?.toBoolean() ?: true)
+val cachePush = (System.getenv("GRADLE_CACHE_PUSH") != "false" || System.getenv("CI") == "true")
+
+buildCache {
+    // The local cache is enabled by default, unless you set `GRADLE_CACHE_LOCAL`
+    // to `false`. This can be useful for testing the remote cache specifically.
+    local {
+        isEnabled = enableLocalCache
+    }
+
+    // Here we configure Buildless as the Gradle remote cache. The remote cache is
+    // enabled by default just like the local cache. Unless `GRADLE_CACHE_PUSH` is
+    // set to `false`, artifacts will be pushed up to the cache when they can't be
+    // found via GET.
+    remote<HttpBuildCache> {
+        isEnabled = enableRemoteCache
+
+        // Buildless uses the Expect/Continue pattern to smoothly reject artifacts
+        // which are inefficient to cache (because they are too large).
+        isUseExpectContinue = true
+
+        // If you prefer not to push unless opted-in, change this conditional
+        // to check that `GRADLE_CACHE_PUSH` == `true`. Pushing should always be
+        // enabled in CI in order to prime the cache for developer use.
+        isPush = cachePush
+
+        // The cache endpoint Gradle uses can be customized on a per-job or
+        // per-project basis with the `CACHE_ENDPOINT` variable. This endpoint is
+        // Buildless' global anycast API domain.
+        url = uri(System.getenv("CACHE_ENDPOINT") ?: "https://global.less.build/cache/generic/")
+
+        credentials {
+            username = "apikey"
+            password = System.getenv("BUILDLESS_APIKEY")
+        }
+    }
+}
