ad7d8c5bee5fe510e2b170747a092cef383db6c6
Sam Gammon <sam@elide.ventures>
2023-04-16 05:48:54 -0700

    chore: fixup planetscale brand

--- workers/wellknown/index.mjs
+++ workers/wellknown/index.mjs
@@ -1,6 +1,6 @@
 const descriptor = {
     "schema_version": "v1",
-    "name_for_human": "Planetscale DB Agent",
+    "name_for_human": "PlanetScale",
     "name_for_model": "planetscale",
     "description_for_human": "Plugin for connecting to a Planetscale database and performing queries using natural language.",
     "description_for_model": "Plugin for connecting to a database and performing queries using natural language inputs from the user; the service translates the queries to SQL, then executes the queries, then provides the results back to the AI.",
