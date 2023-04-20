6dae33e12d33584e95b20b7a61dc07d220ce95a7
Sam Gammon <sam@elide.ventures>
2023-04-20 12:56:22 -0700

    chore: remove unused tests

--- src/test/kotlin/io/github/sgammon/PlanetscaleAiTest.kt
+++ src/test/kotlin/io/github/sgammon/PlanetscaleAiTest.kt
@@ -1,12 +1,1 @@
-package io.github.sgammon
-import io.micronaut.runtime.EmbeddedApplication
-import io.micronaut.test.extensions.kotest.annotation.MicronautTest
-import io.kotest.core.spec.style.StringSpec
-
-@MicronautTest
-class PlanetscaleAiTest(private val application: EmbeddedApplication<*>): StringSpec({
-
-    "test the server is running" {
-        assert(application.isRunning)
-    }
-})
+// nothing here at this time
